61b7b135baac8d84c12365890ea8b5fa379f2ba2
jkleum <127959129+jkleum@users.noreply.github.com>
2023-03-15 18:28:02 +0500

    практика

---
+++ 123.js
@@ -0,0 +1,7 @@
+function show_prompt() 
+        { 
+            var name=prompt("Пожалуйста, введите ваше имя",""); 
+            
+            document.getElementById("demo").innerHTML= name+",спасибо, что посетили этот сайт!";
+        
+        }
